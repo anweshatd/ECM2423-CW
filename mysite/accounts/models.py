e6d416f4fd15b3e62c1621898c35b3f4a591a63e
SophieKW <114511500+SophieKW@users.noreply.github.com>
2024-02-22 14:13:08 +0000

    Update models.py

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = models.IntegerField()
+
+
+
+class GameKeeper(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Developer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
