d157b628fcde039d2143de3278f8c8440fdb03a4
Reathonax <reathonax@outlook.com>
2024-02-23 21:02:28 +0000

    Location and Database
    Added GeoDjango location database and ability to add a location to a challenge.

--- mysite/challenges/admin.py
+++ mysite/challenges/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+# from .models import challenge
+
+# from django.contrib.gis.admin import OSMGeoAdmin
 from .models import challenge
+
+# @admin.register(challenge)
+# class ChallengeAdmin(OSMGeoAdmin):
+#     list_display = ('title', 'location')
 
 admin.site.register(challenge)
--- mysite/challenges/models.py
+++ mysite/challenges/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
 class challenge(models.Model):
@@ -7,6 +8,7 @@ class challenge(models.Model):
     # location
     points = models.IntegerField()
     cType = models.CharField(max_length=100)
+    location = models.PointField(blank=True, null=True)
     
     def __str__(self):
         return self.title
