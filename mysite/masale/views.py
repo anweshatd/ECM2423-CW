4dc9fc3c0e75ba9656d342c557aff66837097a47
anweshatd <ad931@exeter.ac.uk>
2024-02-28 16:05:40 +0000

    changes

--- mysite/masale/views.py
+++ mysite/masale/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from accounts.models import Player
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the masale index.")
+def home(request):
+    current_user = request.user
+    if (current_user.is_authenticated): #will create a new player if it doesn't exist
+        try:
+            current_player = Player.objects.get(pk=current_user.id) 
+        except Player.DoesNotExist:
+            newPlayer = Player(user=current_user,points=0)
+            newPlayer.save() 
+
+    return render(request, "home.html")
