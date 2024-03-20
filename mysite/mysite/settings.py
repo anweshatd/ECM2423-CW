8d35f537fdb297f4cbd1dbdc853f9cb060bf0dfd
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2024-03-20 15:52:39 +0000

    fixed issue with button and updated images and updated challenge

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,3 +146,6 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'images')
+MEDIA_URL='/images/'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -10,3 +10,8 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("challenges/", include("challenges.urls"))#urls need fixing!
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
