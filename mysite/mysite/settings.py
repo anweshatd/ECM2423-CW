c4dbd689202d3bafe720fa6c6803c1a7f0357ca2
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2024-03-21 00:40:10 +0000

    updates for heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -158,5 +158,6 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'images')
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'images')
-MEDIA_URL='/images/'import django_heroku
+MEDIA_URL='/images/'
+import django_heroku
 django_heroku.settings(locals())
